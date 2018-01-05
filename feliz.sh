5844a4908a148e25bb53dba3f71b7fab4f512b69
angeltoast <elizabeth@itsliz.net>
2018-01-05 20:41:22 +0000

    modified:   feliz.sh

--- feliz.sh
+++ feliz.sh
@@ -64,6 +64,8 @@ function main {
 }
 
 function the_start { # All user interraction takes place in this function
+
+read -p "WTF?"
   
   while true; do
     set_language                                  # In f-vars.sh - Use appropriate language file
